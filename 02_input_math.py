e10031d1758f35ae13ab4c960a66ed910bbe75c2
abdulmohiez <abdulmohiez@rhs.school.nz>
2025-02-17 10:15:06 +1300

    Add files via upload
    I have uploaded a input_math file in which we designed a program where we did all sorts of basic calculation and print out the total

---
+++ 02_input_math.py
@@ -0,0 +1,112 @@
+Skip to main content
+Google Classroom
+Classroom
+EXDTE
+Home
+Calendar
+Enrolled
+To do
+E
+EXDTE
+E
+EXLIT
+Level 1 Literacy Explore
+Archived classes
+Settings
+Stream
+Classwork
+People
+Classwork for EXDTE
+All topics
+Important Information
+Important Information
+Material
+Skill_Building
+Edited Yesterday
+By the end of Week 3 - You should have completed Part 2 of Grok Academy
+Spend half an hour each Block completing this - this is your starter and finisher for the day
+
+Here are the files you should have saved in your Google Drive for helpful information
+Variables
+Comments
+Math
+Both files should have been saved in your Google Drive Folder and uploaded (committed) to Github
+
+Another way instead of using .format, is using the f string that you have been learning about in Grok Academy
+
+print(f"{num_1} + {num_2} = {add}")
+
+01_variables.py
+Text
+
+02_input_math.py
+Text
+
+Github.png
+Image
+
+Google_Drive.png
+Image
+Material
+WEEK TWO/THREE - What is being covered - The Basics
+Edited Yesterday
+Material
+WEEK ONE SLIDES & INFO
+Posted 9 Feb
+Material
+Starting out - Please Read
+Posted 10 Feb
+Check Ins
+Check Ins
+Assignment
+Input Loops
+No due date
+Achievement Standards
+Achievement Standards
+Material
+AS92004
+Posted 9 Feb
+Material
+AS91894
+Posted 9 Feb
+#Comment your code (ie: # this is a comment)
+#User input can be stored in variables and then manipulated
+#We can use the 'dot format' method to control exactly how our output appears.
+#If you enter text when a number is expected, the program will crash 
+#(unless you implement code to handle the error correctly)
+
+# get input
+
+# ask user for name
+name = input("What is your name? ")
+
+# ask user for two numbers
+
+# add numbers together
+
+# multiply numbers together
+
+# greet user and display math
+print('Hello')
+print("Hello", name)
+
+# get input
+
+# ask user for name
+name = input("What is your name? ")
+
+# ask user for two numbers
+num_1 = int(input("What is your favourite number? "))
+num_2 = int(input("What is your second favourite number?"))
+
+# add numbers together
+add = num_1 + num_2
+
+# multiply numbers together
+
+# greet user and display math
+print("Hello", name)
+
+print("{} + {} = {}".format(num_1, num_2, add))
+02_input_math.py
+Displaying 02_input_math.py.
