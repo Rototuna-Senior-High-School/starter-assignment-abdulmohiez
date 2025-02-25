84a596bbcd3bde9416c5b9b55f8a5dd797f53b3b
abdulmohiez <abdulmohiez@rhs.school.nz>
2025-02-25 09:44:08 +0500

    Update 02_LU_generate_token.py
    I simplified the program a little bit from before

--- 02_LU_generate_token.py
+++ 02_LU_generate_token.py
@@ -1,25 +1,19 @@
-#ask user how much would he like to spend must be greater and equla to 1$ and less than or equal to 10$
-
-amount_spend = int(input("How much would you like to spend? "))
-
-if amount_spend <= 0 or amount_spend > 10 :
- print("Please enter amount of money you are willing to spend between 1$ and 10$")
-else:
- input("You are good to go. Press <Enter> to proceed.")
-
- 
 import random
 
-# List of characters
-characters = ['Unicorn', 'Horse', 'Zebra', 'Donkey']
+# Ask user for amount to spend
+amount_spend = int(input("How much would you like to spend? "))
 
-# List of corresponding weights (probabilities) for each character
-weights = [0.01, 0.4, 0.4, 0.19]  # The sum of weights should ideally be 1, but it's not mandatory
+# Validate amount
+if amount_spend < 1 or amount_spend > 10:
+    print("Please enter an amount between $1 and $10.")
+else:
+    input("You are good to go. Press <Enter> to proceed.")
 
-# Define the number of tokens to generate
-num_tokens = amount_spend
+    # List of characters
+    characters = ['Unicorn', 'Horse', 'Zebra', 'Donkey']
+    # Corresponding probabilities for each character
+    weights = [0.01, 0.4, 0.4, 0.19]
 
-# Randomly select 'num_tokens' tokens from the list
-generated_tokens = random.choices(characters, k=num_tokens)
-
-print("Generated Tokens:", generated_tokens)
+    # Generate tokens based on the amount spent
+    generated_tokens = random.choices(characters, weights=weights, k=amount_spend)
+    print("Generated Tokens:", generated_tokens)kens)
