ac795304afea995b43e65152b307c708415adb6a
abdulmohiez <abdulmohiez@rhs.school.nz>
2025-03-10 10:33:48 +1300

    Update 01_LU_how_much.py

--- 01_LU_how_much.py
+++ 01_LU_how_much.py
@@ -1,10 +1,6 @@
 import random
 
-# Ask user for amount to spend
-amount_spend = int(input("How much would you like to spend? "))
+amount_spend = int(input("Please enter the amount that you want to spend." ))
 
-# Validate amount
-if amount_spend < 1 or amount_spend > 10:
-    print("Please enter an amount between $1 and $10.")
-else:
-    input("You are good to go. Press <Enter> to proceed.")
+if amount_spend < 0 or amount_spend > 10:
+ print("Please enter the amount between 0 and 10$.")
