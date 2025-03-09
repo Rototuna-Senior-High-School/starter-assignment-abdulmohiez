be0844e6ec2c26b2cba63f7c73d901c5b04396ce
abdulmohiez <abdulmohiez@rhs.school.nz>
2025-03-10 10:33:19 +1300

    Update 02_LU_generate_token.py

--- 02_LU_generate_token.py
+++ 02_LU_generate_token.py
@@ -1,19 +1,12 @@
 import random
 
-# Ask user for amount to spend
-amount_spend = int(input("How much would you like to spend? "))
+amount_spend = int(input("Please enter the amount that you want to spend." ))
 
-# Validate amount
-if amount_spend < 1 or amount_spend > 10:
-    print("Please enter an amount between $1 and $10.")
-else:
-    input("You are good to go. Press <Enter> to proceed.")
+if amount_spend < 0 or amount_spend > 10:
+ print("Please enter the amount between 0 and 10$.")
 
-    # List of characters
-    characters = ['Unicorn', 'Horse', 'Zebra', 'Donkey']
-    # Corresponding probabilities for each character
-    weights = [0.01, 0.4, 0.4, 0.19]
+list_of_characters = ["Unicorn", "Horse","Zebra","Donkey"]
+weights = [0.01, 0.4, 0.4, 0.19]
 
-    # Generate tokens based on the amount spent
-    generated_tokens = random.choices(characters, weights=weights, k=amount_spend)
-    print("Generated Tokens:", generated_tokens)kens)
+generated_token = random.choices(list_of_characters ,weights=weights, k=amount_spend)
+print("Generated Tokens:", generated_token)
